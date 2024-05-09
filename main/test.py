e44a7324b039627d0772063daf08faf1a8b55baa
nanosize <nao888.yt@gmail.com>
2024-05-09 20:15:12 +0900

    add

--- main/test.py
+++ main/test.py
@@ -5,26 +5,9 @@ root.title("window")     #ウィンドウタイトルを設定
 root.geometry("300x300") #ウィンドウサイズを設定
 
 frame_1 = Frame(root, bd=4, relief=GROOVE)
-frame_2 = Frame(root, bd=4, relief=GROOVE)
-frame_3 = Frame(root, bd=4, relief=GROOVE)
- 
-# ボタンwidgetを設定
-button_1 = Button(frame_1, text='Button1-a')
-button_2 = Button(frame_1, text='Button2-bb')
-button_3 = Button(frame_1, text='Button3-ccc')
-button_4 = Button(frame_2, text='Button4-dddd')
-button_5 = Button(frame_2, text='Button5-eeeee')
-button_6 = Button(frame_3, text='Button6-ffffff')
- 
-# widgetの配置を設定
-frame_1.grid(row=0, column=0)
-frame_2.grid(row=1, column=1)
-frame_3.grid(row=2, column=2)
+button_1 = Label(frame_1, text='test')
+
+frame_1.pack()
 button_1.pack()
-button_2.pack()
-button_3.pack()
-button_4.pack()
-button_5.pack()
-button_6.pack()
 
 root.mainloop()
--- main/window.py
+++ main/window.py
@@ -1,25 +1,32 @@
 from tkinter import *
 from tkinter import ttk
+import requests 
 
+#window作成
 root = Tk()
 root.title('グローバルIP表示')
 root.geometry('300x200')
 
+#タイトル
 Label = ttk.Label(root,text='現在のGIP',padding = (5,10),font = ('gothic',20))
-button = ttk.Button(root,text = 'OK')
-
 Label.pack()
+
+#メインコードの実装
+def get_gip():
+    global gip
+    gip = requests.get('http://api.ipify.org/') 
+    #print(gip.text)
+get_gip()
+print(gip.text)
+
+#gip表示
+frame_1 = Frame(root, bd=4, relief=GROOVE,pady=10, padx=100)
+GIP_label = ttk.Label(frame_1, text=gip)
+frame_1.pack()
+GIP_label.pack()
+
+button = ttk.Button(root,text = 'OK')
 button.pack()
-
-frame = ttk.Frame(root,padding=10)
-frame2 = ttk.Frame(
-    frame, width=200, height=100,
-    borderwidth=10, relief='sunken')
-Label_frame = ttk.Label(root,text= "aaa")
-frame.pack()
-frame2.pack()
-
-
 
 #windowの表示
 root.mainloop()
