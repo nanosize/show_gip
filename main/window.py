93682952d5f9b4b8b2385da218fdcb31919aa637
nanosize <nao888.yt@gmail.com>
2024-05-09 20:30:58 +0900

    add window

--- main/window.py
+++ main/window.py
@@ -8,7 +8,7 @@ root.title('グローバルIP表示')
 root.geometry('300x200')
 
 #タイトル
-Label = ttk.Label(root,text='現在のGIP',padding = (5,10),font = ('gothic',20))
+Label = ttk.Label(root,text='現在のグローバルIP',padding = (5,10),font = ('gothic',20))
 Label.pack()
 
 #メインコードの実装
@@ -17,16 +17,16 @@ def get_gip():
     gip = requests.get('http://api.ipify.org/') 
     #print(gip.text)
 get_gip()
-print(gip.text)
+print("line20" + gip.text)
 
 #gip表示
-frame_1 = Frame(root, bd=4, relief=GROOVE,pady=10, padx=100)
-GIP_label = ttk.Label(frame_1, text=gip)
+frame_1 = Frame(root, bd=4, relief=GROOVE,pady=10, padx=80)
+GIP_label = ttk.Label(frame_1, text=gip.text)
 frame_1.pack()
 GIP_label.pack()
 
 button = ttk.Button(root,text = 'OK')
-button.pack()
+#button.pack()
 
 #windowの表示
 root.mainloop()
