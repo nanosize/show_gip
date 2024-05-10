6d00f35f316539f084b997efadb7e1c47cbbce94
nanosize <nao888.yt@gmail.com>
2024-05-10 16:57:37 +0900

    change

--- setup.py
+++ setup.py
@@ -1,71 +1,17 @@
-import sys
 from cx_Freeze import setup, Executable
 
+# Dependencies are automatically detected, but it might need
+# fine tuning.
+build_options = {'packages': ['requests'], 'excludes': []}
 
-# ----------------------------------------------------------------
-# 基本情報
-# ----------------------------------------------------------------
-name = "Show-Gip"
-version = "0.1"
-description = "自分のグローバルIPを簡単に確認することができます"
-author = "Nanosize"
-license= 'MIT'
-url = "https://netchira.github.io/"
-icon = "icons/gip_icon.ico"
+base = 'gui'
 
-# アプリケーションの種別を選択
-if sys.platform == "win32":
-    base = "Win32GUI"
+executables = [
+    Executable('main.py', base=base, target_name = 'Show-Gip',icon= 'icons/gip_icon.ico')
+]
 
-# UUIDは一度決めたら変更しない
-upgrade_code = "{a2b30d2a-0e91-11ef-9e21-242fd016a5e8}"
-
-# ----------------------------------------------------------------
-# セットアップ処理
-# ----------------------------------------------------------------
-shortcut_table = [
-    ('DesktopShortcut',          # Shortcut
-     'DesktopFolder',            # Directory_
-     "CompareTxt",               # Name
-     'TARGETDIR',                # Component_
-     '[TARGETDIR]CompareTxt.exe',# Target
-     'license',                  # license
-     None,                       # Description
-     None,                       # Hotkey
-     None,                       # Icon
-     None,                       # IconIndex
-     None,                       # ShowCmd
-     'TARGETDIR',                # WkDir
-    )
-    ]
-
-# Table dictionary
-msi_data = {'Shortcut': shortcut_table}
-
-# 追加モジュールで必要なものを packages に入れる
-build_exe_options = {'packages': ['requests'],
-                     'excludes': [],
-                     'includes': [],
-                     'include_files': ["icons/"]
-}
-
-bdist_msi_options = {'upgrade_code': upgrade_code,
-                     'add_to_path': False,
-                     'data': msi_data
-}
-
-options = {
-    'build_exe': build_exe_options,
-    'bdist_msi': bdist_msi_options
-}
-
-
-# セットアップ処理
-setup(name=name,
-      version=version,
-      author=author,
-      url=url,
-      description=description,
-      options=options,
-      executables=[Executable(script="main.py",icon=icon)]
-      )
+setup(name='Show-Gip',
+      version = '1.0',
+      description = 'Show your global IP address.',
+      options = {'build_exe': build_options},
+      executables = executables)
